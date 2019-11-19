8a6f006b5b8de00586cf6743157d012ed3494f2e
wkd3475 <wkd3475@gmail.com>
2019-11-19 15:37:25 +0900

    start

---
+++ RPC_server.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+	"os"
+)
+
+type Calc int // RPC 서버에 등록하기 위해 임의의 타입으로 정의
+
+type Args struct { // 매개변수
+	Root string
+}
+
+type Reply struct { // 리턴값
+	Files []string
+}
+
+func OSReadDir(root string) ([]string, error) {
+	var files []string
+	f, err := os.Open(root)
+	if err != nil {
+		return files, err
+	}
+	fileInfo, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		return files, err
+	}
+
+	for _, file := range fileInfo {
+		files = append(files, file.Name())
+	}
+	return files, nil
+}
+
+func (c *Calc) Ls(args *Args, reply *Reply) error {
+	var (
+		files []string
+		err   error
+	)
+	location := args.Root
+	files, err = OSReadDir(location)
+	if err != nil {
+		panic(err)
+	}
+
+	reply.Files = files
+
+	return nil
+}
+
+func main() {
+	rpc.Register(new(Calc))               // Calc 타입의 인스턴스를 생성하여 RPC 서버에 등록
+	ln, err := net.Listen("tcp", ":6000") // TCP 프로토콜에 6000번 포트로 연결을 받음
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer ln.Close() // main 함수가 종료되기 직전에 연결 대기를 닫음
+
+	for {
+		conn, err := ln.Accept() // 클라이언트가 연결되면 TCP 연결을 리턴
+
+		if conn != nil {
+			fmt.Println("hello")
+		}
+		if err != nil {
+			continue
+		}
+		defer conn.Close() // main 함수가 끝나기 직전에 TCP 연결을 닫음
+
+		go rpc.ServeConn(conn) // RPC를 처리하는 함수를 고루틴으로 실행
+	}
+}
