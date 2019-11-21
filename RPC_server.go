78e0f32667e699f84d617c854bc7856ba7ad2384
wkd3475 <wkd3475@gmail.com>
2019-11-21 12:04:24 +0900

    update

--- RPC_server.go
+++ RPC_server.go
@@ -52,16 +52,16 @@ func (c *Request) Ls(args *Args, reply *Reply) error {
 }
 
 func main() {
-	rpc.Register(new(Request))            // Request 타입의 인스턴스를 생성하여 RPC 서버에 등록
-	ln, err := net.Listen("tcp", ":6000") // TCP 프로토콜에 6000번 포트로 연결을 받음
+	rpc.Register(new(Request))
+	ln, err := net.Listen("tcp", ":6000")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer ln.Close() // main 함수가 종료되기 직전에 연결 대기를 닫음
+	defer ln.Close()
 
 	for {
-		conn, err := ln.Accept() // 클라이언트가 연결되면 TCP 연결을 리턴
+		conn, err := ln.Accept()
 
 		if conn != nil {
 			fmt.Println("hello")
@@ -69,8 +69,8 @@ func main() {
 		if err != nil {
 			continue
 		}
-		defer conn.Close() // main 함수가 끝나기 직전에 TCP 연결을 닫음
+		defer conn.Close()
 
-		go rpc.ServeConn(conn) // RPC를 처리하는 함수를 고루틴으로 실행
+		go rpc.ServeConn(conn)
 	}
 }
